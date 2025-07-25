86eebbb3d81c7388f50ebf707d2842cfcdca82d8
David Ajaero <davidtertial@gmail.com>
2025-07-25 13:08:06 +0100

    Added my MenuPage.kt

--- AndroidStudioProjects/Filtered/app/src/main/java/com/frontendmasters/filtered/pages/MenuPage.kt
+++ AndroidStudioProjects/Filtered/app/src/main/java/com/frontendmasters/filtered/pages/MenuPage.kt
@@ -8,3 +8,47 @@ import androidx.compose.ui.tooling.preview.Preview
 fun MenuPage(){
 
 }
+
+fun Double.format(digits: Int) = "%.${digits}f".format(this)
+
+
+@Composable
+fun ProductItem(product: Product, onAdd: (Product)->Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.black_coffee),
+            contentDescription = "Image for ${product.name}",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Column {
+                Text(product.name, fontWeight = FontWeight.Bold)
+                Text("$${product.price.format(2)} ea")
+            }
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Alternative1,
+                    contentColor = Color.White
+                ),
+                onClick = {
+                    onAdd(product)
+                },
+            ) {
+                Text("Add")
+            }
+        }
+    }
+}
